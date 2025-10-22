c70061b9c88f74a9a8f9bb1fb9cab17db093848e
kareem gamal <kareem.gamal142000@gmail.com>
2025-10-22 15:58:57 +0300

    Add Station Repo and Interface

---
+++ DAL/Repo/Station/IStationRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo.Station
+{
+    public interface IStationRepo
+    {
+        Task<IEnumerable<Entities.Station>> GetAllStationsAsync();
+        Task<Entities.Station?> GetStationByIdAsync(int id);
+        Task<Entities.Station?> GetStationByCodeAsync(string stationCode);
+        Task<IEnumerable<Entities.Station>> GetStationsByNameAsync(string name);
+        Task<string> AddTrainAsync(Entities.Station station);
+        Task<string> UpdateStation(Entities.Station station);
+        Task<string> DeleteStationByIdAsync(int id);
+    }
+}
---
+++ DAL/Repo/Station/StationRepo.cs
@@ -0,0 +1,70 @@
+﻿using DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repo.Station
+{
+    public class StationRepo : IStationRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StationRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Entities.Station>> GetAllStationsAsync()
+        {
+            return await _context.Stations.ToListAsync();
+        }
+
+        public async Task<Entities.Station?> GetStationByIdAsync(int id)
+        {
+            return await _context.Stations.FindAsync(id);
+        }
+
+        public async Task<Entities.Station?> GetStationByCodeAsync(string stationCode)
+        {
+            return await _context.Stations.FirstOrDefaultAsync(s => s.StationCode == stationCode);
+        }
+
+        public async Task<IEnumerable<Entities.Station>> GetStationsByNameAsync(string name)
+        {
+            string normalizedName = name.ToLower();
+
+            return await _context.Stations
+                .Where(s => s.StationNameEN.ToLower().Contains(normalizedName) ||
+                            s.StationNameAR.Contains(normalizedName))
+                .ToListAsync();
+        }
+        public async Task<string> AddTrainAsync(Entities.Station station)
+        {
+            await _context.Stations.AddAsync(station);
+            await _context.SaveChangesAsync();
+            return "Station added successfully";
+        }
+
+        public async Task<string> UpdateStation(Entities.Station station)
+        {
+            _context.Stations.Update(station);
+            await _context.SaveChangesAsync();
+            return "Station updated successfully";
+        }
+
+        public async Task<string> DeleteStationByIdAsync(int id)
+        {
+            var station = await _context.Stations.FindAsync(id);
+            if (station != null)
+            {
+                _context.Stations.Remove(station);
+                await _context.SaveChangesAsync();
+                return "Station deleted successfully";
+            }
+            return "Station not found";
+
+        }
+    }
+}
