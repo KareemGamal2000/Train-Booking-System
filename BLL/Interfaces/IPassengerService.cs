c3a7eb42d8eae36dd10db67f42736fcbb46843c5
Rewan Elbahnsawy <rewanmohamed491@gmail.com>
2025-11-11 19:08:18 +0200

    add passenger interface

---
+++ BLL/Interfaces/IPassengerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Dtos;
+
+namespace Domain.Interfaces
+{
+    public interface IPassengerService
+    {
+        Task<IEnumerable<PassengerDto>> GetAllAsync();
+        Task<PassengerDto?> GetByIdAsync(int id);
+        Task<string> AddAsync(CreatePassengerDto passengerDto);
+        Task<string> UpdateAsync(UpdatePassengerDto passengerDto);
+        Task<string> DeleteAsync(int id);
+    }
+}
---
+++ BLL/Services/PassengerService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Data.Entities;
+using Data.Repository.Passenger;
+using Domain.Dtos;
+using Domain.Interfaces;
+
+namespace Domain.Services
+{
+    public class PassengerService : IPassengerService
+    {
+        private readonly IPassengerRepo _repo;
+        private readonly IMapper _mapper;
+
+        public PassengerService(IPassengerRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<PassengerDto>> GetAllAsync()
+        {
+            var passengers = await _repo.GetAllAsync();
+            return _mapper.Map<IEnumerable<PassengerDto>>(passengers);
+        }
+
+        public async Task<PassengerDto?> GetByIdAsync(int id)
+        {
+            var passenger = await _repo.GetByIdAsync(id);
+            return _mapper.Map<PassengerDto>(passenger);
+        }
+
+        public async Task<string> AddAsync(CreatePassengerDto dto)
+        {
+            var passenger = _mapper.Map<Passenger>(dto);
+            return await _repo.AddAsync(passenger);
+        }
+
+        public async Task<string> UpdateAsync(UpdatePassengerDto dto)
+        {
+            var passenger = _mapper.Map<Passenger>(dto);
+            return await _repo.UpdateAsync(passenger);
+        }
+
+        public async Task<string> DeleteAsync(int id)
+        {
+            return await _repo.DeleteAsync(id);
+        }
+    }
+}
