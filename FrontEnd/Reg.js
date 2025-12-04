2a317e02cc6e6125a2070a041e45afd17d1d1eed
İbrahim mohamed <145792609+Ibrahimrefaat123@users.noreply.github.com>
2025-12-04 20:02:04 +0200

    Add files via upload

---
+++ FrontEnd/Reg.js
@@ -0,0 +1,93 @@
+
+
+const form = document.getElementById('register-form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+const firstNameError = document.getElementById('firstName-error');
+const lastNameError = document.getElementById('lastName-error');
+const phoneError = document.getElementById('phone-error');
+const emailError = document.getElementById('email-error');
+const passwordError = document.getElementById('password-error');
+
+const dateOfBirth = document.getElementById("dateOfBirth").value;
+
+if (!firstName || !lastName || !dateOfBirth || !gender || !phone || !email || !password) {
+    alert("من فضلك اكمل جميع الحقول المطلوبة");
+    return;
+}
+
+const data = {
+    FirstName: firstName,
+    LastName: lastName,
+    DateOfBirth: dateOfBirth, 
+    Gender: gender,
+    PhoneNumber: phone,
+    Email: email,
+    Password: password
+};
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let valid = true;
+
+    
+    firstNameError.textContent = "";
+    lastNameError.textContent = "";
+    phoneError.textContent = "";
+    emailError.textContent = "";
+    passwordError.textContent = "";
+
+    
+    if(firstName.value.trim() === ""){
+        firstNameError.textContent = "يرجى إدخال الاسم الأول";
+        valid = false;
+    }
+
+
+    if(lastName.value.trim() === ""){
+        lastNameError.textContent = "يرجى إدخال الاسم الأخير";
+        valid = false;
+    }
+
+    
+    const phonePattern = /^[0-9]{10,15}$/;
+    if(phone.value.trim() === ""){
+        phoneError.textContent = "يرجى إدخال رقم الموبايل";
+        valid = false;
+    } else if(!phone.value.match(phonePattern)){
+        phoneError.textContent = "رقم الموبايل غير صالح";
+        valid = false;
+    }
+
+    
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(email.value.trim() === ""){
+        emailError.textContent = "يرجى إدخال البريد الإلكتروني";
+        valid = false;
+    } else if(!email.value.match(emailPattern)){
+        emailError.textContent = "صيغة البريد الإلكتروني غير صحيحة";
+        valid = false;
+    }
+
+    
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%&*!?])[A-Za-z\d@#$%&*!?]{8,}$/;
+    if(password.value.trim() === ""){
+        passwordError.textContent = "يرجى إدخال كلمة المرور";
+        valid = false;
+    } else if(!passwordPattern.test(password.value)){
+        passwordError.textContent = "يجب أن تحتوي على 8 أحرف وحرف كبير وصغير ورقم ورمز";
+        valid = false;
+    }
+
+    if(valid){
+if(valid){
+            window.location.href = "Home.html" ;
+}
+     
+    }
+});
